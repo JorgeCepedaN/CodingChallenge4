914fd2a5885de6fc5b1d19eb01feb8e07aa759b8
Jorge Cepeda <Jorge.cepeda@nagarro.com>
2024-01-28 16:48:58 -0600

    Feat: update the filter by year

--- src/components/vehicles.js
+++ src/components/vehicles.js
@@ -33,6 +33,7 @@ export default function Vehicles({ datasetId }) {
             
                         setVehiclesDetails(sortedVehicles);
                         setVehiclesDetailsCopy(sortedVehicles);
+                        setIsLoading(false);
                     })
                     .catch((error) => {
                         console.error("Error while obtaining vehicle details: ", error);
@@ -56,7 +57,6 @@ export default function Vehicles({ datasetId }) {
                 console.error("Error while fetching vehicles Ids: ", error);
             })
             .finally(() => {
-                setIsLoading(false);
             });
     }, [datasetId]);
 
@@ -112,7 +112,16 @@ export default function Vehicles({ datasetId }) {
                 <table className="vehicle-table">
                     <thead>
                         <tr>
-                            <th>Year</th>
+                            <th onClick={handleSort} className="sortable-header">
+                                Year
+                                <svg
+                                    className={`sort-icon ${sortOrder === "asc" ? "asc" : "desc"}`}
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 320 512"
+                                >
+                                    <path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z" />
+                                </svg>
+                            </th>
                             <th>Make</th>
                             <th>Model</th>
                         </tr>
@@ -122,9 +131,6 @@ export default function Vehicles({ datasetId }) {
                     </tbody>
                 </table>
             )}
-            <button className="sort-button" onClick={handleSort}>
-                Sort {sortOrder === "asc" ? "Ascending" : "Descending"}
-            </button>
         </div>
     );
 
